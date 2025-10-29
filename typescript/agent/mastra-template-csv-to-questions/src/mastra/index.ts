ee540d75a023343034702a7cd62457c066c584fa
Gustavo Santos <gustavo.santos@galileo.ai>
2025-10-29 14:55:20 -0300

    feat: support latest version of mastra using the observability property

--- typescript/agent/mastra-template-csv-to-questions/src/mastra/index.ts
+++ typescript/agent/mastra-template-csv-to-questions/src/mastra/index.ts
@@ -1,17 +1,14 @@
-import {
-  OpenInferenceOTLPTraceExporter,
-  isOpenInferenceSpan,
-} from "@arizeai/openinference-mastra";
+import { SamplingStrategyType } from '@mastra/core/ai-tracing';
 import { Mastra } from '@mastra/core/mastra';
 import { LibSQLStore } from '@mastra/libsql';
 import { PinoLogger } from '@mastra/loggers';
-import { env } from "node:process";
+import { OtelExporter } from "@mastra/otel-exporter";
+import 'dotenv/config';
+import { env } from 'node:process';
 import { csvQuestionAgent } from './agents/csv-question-agent';
 import { csvSummarizationAgent } from './agents/csv-summarization-agent';
 import { textQuestionAgent } from './agents/text-question-agent';
 import { csvToQuestionsWorkflow } from './workflows/csv-to-questions-workflow';
-
-console.log("Starting Mastra...");
 
 export const mastra = new Mastra({
   workflows: { csvToQuestionsWorkflow },
@@ -28,23 +25,27 @@ export const mastra = new Mastra({
     name: 'Mastra',
     level: 'info',
   }),
-  telemetry: {
-    serviceName: "openinference-mastra-agent",
-    enabled: true, // enable telemetry for this project
-    sampling: {
-      type: "always_on", // this makes it always sample traces so it's easier to test
-    },
-    export: {
-      type: "custom", // we need to use the custom open inference exporter
-      exporter: new OpenInferenceOTLPTraceExporter({
-        url: `${(env.GALILEO_CONSOLE_URL ?? "https://app.galileo.ai")}/api/galileo/otel/traces`,// this is the endpoint for the otel traces
-        headers: {
-          "Galileo-API-Key": env.GALILEO_API_KEY ?? "your-galileo-api-key", // your galileo api key
-          "project": env.GALILEO_PROJECT ?? "your-galileo-project", // your galileo project
-          "logstream": env.GALILEO_LOG_STREAM ?? "default", // your galileo log stream
-        },
-        spanFilter: isOpenInferenceSpan,
-      }),
-    },
+  observability: {
+    configs: {
+      otel: {
+        sampling: { type: SamplingStrategyType.ALWAYS },
+        serviceName: 'galileo-mastra-agent',
+        exporters: [
+          new OtelExporter({
+            provider: {
+              custom: {
+                endpoint: env.GALILEO_CONSOLE_URL,
+                headers: {
+                  'Galileo-API-Key': process.env.GALILEO_API_KEY ?? '',
+                  'project': env.GALILEO_PROJECT ?? '',
+                  'logstream': env.GALILEO_LOG_STREAM ?? '',
+                },
+                protocol: 'http/protobuf',
+              }
+            },
+          }),
+        ]
+      }
+    }
   },
 });
