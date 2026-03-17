a3fec26ba793463cf6589f554084624a7c852a90
xke <xian@galileo.ai>
2026-03-17 23:11:54 +0000

    fix lint

--- python/logging-samples/galileologger/metadata-example.py
+++ python/logging-samples/galileologger/metadata-example.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from galileo import GalileoLogger
-from galileo.config import GalileoPythonConfig # For displaying the log stream URL
+from galileo.config import GalileoPythonConfig  # For displaying the log stream URL
 import openai
 from dotenv import load_dotenv
 
@@ -15,29 +15,16 @@ input_prompt = "Explain the following topic succinctly: Newton's First Law"
 output_answer = "A body remains at rest or moves in a straight line with constant speed unless acted on by a nonzero net external force."
 
 # Define optional metadata
-metadata = {"experimentNumber": "1",
-            "promptVersion": "0.0.1",
-            "field": "physics"}
+metadata = {"experimentNumber": "1", "promptVersion": "0.0.1", "field": "physics"}
 
 # Start a Galileo session with optional metadata
-session_id = logger.start_session(
-    name="Test session", 
-    metadata=metadata
-)
+session_id = logger.start_session(name="Test session", metadata=metadata)
 
 # Initialize a new Trace with optional metadata
-trace = logger.start_trace(
-    input=input_prompt,
-    metadata=metadata
-)
+trace = logger.start_trace(input=input_prompt, metadata=metadata)
 
 # Add an LLM span to the trace with optional metadata
-logger.add_llm_span(
-    input=[{"role": "system", "content": input_prompt}],
-    output=output_answer,
-    model="gpt-5-mini",
-    metadata=metadata
-)
+logger.add_llm_span(input=[{"role": "system", "content": input_prompt}], output=output_answer, model="gpt-5-mini", metadata=metadata)
 
 # Conclude the trace with the final output
 logger.conclude(output_answer)
